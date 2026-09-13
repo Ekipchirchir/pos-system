d3cf14af270aaeac51e2043d48b7577be58dfa9b
Ekipchirchir <manuuchirchir50@gmail.com>
2026-09-13 16:52:06 +0300

    fix: update calendar ui component

--- frontend/components/date-picker.tsx
+++ frontend/components/date-picker.tsx
@@ -18,7 +18,7 @@ export default function DatePicker() {
   return (
     <Popover>
       <PopoverTrigger render={<Button variant="outline" className={cn(
-                      "w-[240px] justify-start text-left font-normal",
+                      "w-60 justify-start text-left font-normal",
                       !date && "text-muted-foreground"
                     )} />}><CalendarIcon className="mr-2 h-4 w-4" />{date ? format(date, "PPP") : <span>Pick a date</span>}</PopoverTrigger>
       <PopoverContent className="w-auto p-0" align="start">
